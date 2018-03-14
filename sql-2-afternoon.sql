54baf4924e7d3c8529a2c4ff017abf494614bdc7
bkchu <brandon.k.chung@gmail.com>
2018-03-14 13:23:05 -0500

    6

--- sql-2-afternoon.sql
+++ sql-2-afternoon.sql
@@ -9,3 +9,8 @@ FROM Invoice i
 JOIN InvoiceLine il
 ON i.invoiceid = il.invoiceid
 WHERE il.unitprice > 0.99
+
+SELECT c.firstname, c.lastname, e.firstname, e.lastname
+FROM Customer c
+JOIN Employee e
+ON c.supportrepid = e.employeeid
