3253d7511238a58e5146f1d906e96fd5222fff9c
bkchu <brandon.k.chung@gmail.com>
2018-03-14 13:18:31 -0500

    5

--- sql-2-afternoon.sql
+++ sql-2-afternoon.sql
@@ -3,3 +3,9 @@ FROM Invoice i
 JOIN InvoiceLine il
 ON i.invoiceid = il.invoiceid
 WHERE il.unitprice > 0.99
+
+SELECT *
+FROM Invoice i
+JOIN InvoiceLine il
+ON i.invoiceid = il.invoiceid
+WHERE il.unitprice > 0.99
